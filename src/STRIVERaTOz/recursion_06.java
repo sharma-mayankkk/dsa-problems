e176c6b039c84f88b8a97ace34055c9c5d68860c
sharma-mayankkk <br10216030515@gmail.com>
2026-02-19 20:08:27 +0530

    reverse an array by recursion

--- src/STRIVERaTOz/recursion_06.java
+++ src/STRIVERaTOz/recursion_06.java
@@ -1,5 +1,24 @@
 package STRIVERaTOz;
 //Reverse an array
 public class recursion_06 {
+    public static void reverseArray(int[] arr, int l, int r){
 
+        if(l>=r) return;
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+
+        reverseArray(arr,l+1, r-1);
+    }
+    public static void printArr(int[] arr){
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        reverseArray(arr,0,arr.length-1);
+        printArr(arr);
+    }
 }
