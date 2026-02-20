60895742ba76f0877e51553aeaa8e378c09ccb5d
sharma-mayankkk <br10216030515@gmail.com>
2026-02-20 19:00:49 +0530

    remove duplicate elements from the sorted array :easy

---
+++ src/STRIVERaTOz/Array/array_01.java
@@ -0,0 +1,27 @@
+package STRIVERaTOz.Array;
+
+public class array_01 {
+
+    public static int removeDuplicates(int[] arr) {
+        int i = 0;
+        for(int j = 1; j < arr.length; j++){
+            if(arr[i] != arr[j]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+
+    public static void printArr(int[] arr, int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4, 5, 9, 9};
+        int newLen = removeDuplicates(arr);
+        printArr(arr, newLen);
+    }
+}
