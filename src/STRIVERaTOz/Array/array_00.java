1c8f7d4ff79f01173a70ccd342abfcd6a538cb8f
sharma-mayankkk <br10216030515@gmail.com>
2026-02-20 18:19:59 +0530

    Check if array is sorted or not: easy

---
+++ src/STRIVERaTOz/Array/array_00.java
@@ -0,0 +1,17 @@
+package STRIVERaTOz.Array;
+
+//Check if the Array is Sorted II
+public class array_00 {
+    public static boolean checkSortedArray(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] >= arr[i - 1]) {
+            } else return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,6,7,9,2};
+        System.out.println(checkSortedArray(arr));;
+    }
+}
