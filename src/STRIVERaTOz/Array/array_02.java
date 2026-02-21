107b5d186c9e1b2fb89e74eefa6b05418c8992b1
sharma-mayankkk <br10216030515@gmail.com>
2026-02-21 15:40:55 +0530

    rotate array by one place.

---
+++ src/STRIVERaTOz/Array/array_02.java
@@ -0,0 +1,22 @@
+package STRIVERaTOz.Array;
+
+//Left Rotate Array by One
+public class array_02 {
+    public static void rotateArr(int[] arr){
+        int temp = arr[0];
+        for(int i=1; i<arr.length; i++){
+            arr[i-1] = arr[i];
+        }
+        arr[arr.length-1] = temp;
+    }
+    public static  void printArr(int[] arr){
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        rotateArr(arr);
+        printArr(arr);
+    }
+}
