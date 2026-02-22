50863280a04d3d8ffd495c0133b47bd8be97b4d3
sharma-mayankkk <br10216030515@gmail.com>
2026-02-22 17:04:55 +0530

    Leetcode Problem no.:283 (easy) move zeroes with optimal solution.

---
+++ src/LEETCODE/leetCode_283.java
@@ -0,0 +1,30 @@
+package LEETCODE;
+
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+public class leetCode_283 {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return;
+
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
