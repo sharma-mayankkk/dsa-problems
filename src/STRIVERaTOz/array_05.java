877c158fadad1f1891e1c3108541afb7ee9364b6
sharma-mayankkk <br10216030515@gmail.com>
2026-02-22 17:16:23 +0530

    linear search in array.

---
+++ src/STRIVERaTOz/array_05.java
@@ -0,0 +1,18 @@
+package STRIVERaTOz;
+
+public class array_05 {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 55, 11};
+        System.out.println(linearSearch(arr, 11));
+        ;
+    }
+}
