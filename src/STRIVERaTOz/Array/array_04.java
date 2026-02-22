4ad22ed7938a06dceae7b07ddd779d72b0996714
sharma-mayankkk <br10216030515@gmail.com>
2026-02-22 16:52:05 +0530

    move zeroes in the array to the last place with BF and optimal solution:

---
+++ src/STRIVERaTOz/Array/array_04.java
@@ -0,0 +1,58 @@
+package STRIVERaTOz.Array;
+//Problem Statement: You are given an array of integers, your task is to move all the zeros in the array to the end of the array and move non-negative integers to the front by maintaining their order.
+public class array_04 {
+
+    //Bruteforce approach:
+    public static void moveZeroes(int[] arr, int n){
+        int[] temp = new int[n];
+        int k =0;
+
+        //copying non-zero element in temp array
+        for(int i: arr){
+            if(i!=0){
+                temp[k] = i;
+                k++;
+            }
+        }
+
+        //copy temp back to original array
+        for(int i=0; i<k; i++){
+            arr[i] = temp[i];
+        }
+
+        //filling last places with 0
+        for(int i=k; i<n; i++){
+            arr[i] = 0;
+        }
+    }
+
+    //Optimal Solution:
+    public static void moveZerosToLast(int[] arr, int n){
+        int j = -1;
+        for(int i=0; i<n; i++){
+            if(arr[i] ==0){
+                j = i;
+                break;
+            }
+        }
+
+        for(int i = j+1; i<n; i++){
+            if(arr[i] !=0){
+                int temp = arr[i];
+                arr[i] =arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+    public static void printArr ( int[] arr){
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,5,7,0,0,8};
+        moveZerosToLast(arr,arr.length);
+        printArr(arr);
+    }
+}
