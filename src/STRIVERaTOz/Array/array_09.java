3cad9af792b5f62c4a5b94aa2abda4933a845dfc
sharma-mayankkk <br10216030515@gmail.com>
2026-02-24 16:40:12 +0530

    Maximum consecutive ones in array with optimal solution.

---
+++ src/STRIVERaTOz/Array/array_09.java
@@ -0,0 +1,28 @@
+package STRIVERaTOz.Array;
+
+import javax.swing.plaf.IconUIResource;
+
+//Find Maximum consecutive ones in the array:
+public class array_09 {
+    public static int maxConOnes(int[] arr) {
+        int max = 0;
+        int count = 0;
+
+        for (int i : arr) {
+            if (i == 1) {
+                count++;
+                if (count > max) {
+                    max = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1};
+        System.out.println(maxConOnes(arr));
+    }
+}
