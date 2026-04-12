04ac67d5c96f0d771d34df305aa599aea21b1cc8
sharma-mayankkk <br10216030515@gmail.com>
2026-04-12 18:21:15 +0530

    next_permutation : find next lexicographically greater permutation with optimal solution

--- src/STRIVERaTOz/Array/Medium/array_05.java
+++ src/STRIVERaTOz/Array/Medium/array_05.java
@@ -2,21 +2,21 @@ package STRIVERaTOz.Array.Medium;
 
 //next_permutation : find next lexicographically greater permutation
 public class array_05 {
-    public static int[] nextPermutation(int[] arr){
+    public static int[] nextPermutation(int[] arr) {
         int n = arr.length;
         int index = -1;
-        for(int i = n-2; i>=0; i--){
-            if(arr[i] < arr[i+1]){
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
                 index = i;
                 break;
             }
         }
-        if(index==-1){
-            reverseArray(arr,0,n-1);
+        if (index == -1) {
+            reverseArray(arr, 0, n - 1);
             return arr;
         }
-        for(int i = n-1; i>index; i-- ){
-            if(arr[i]>arr[index]){
+        for (int i = n - 1; i > index; i--) {
+            if (arr[i] > arr[index]) {
                 int temp = arr[i];
                 arr[i] = arr[index];
                 arr[index] = temp;
@@ -24,24 +24,26 @@ public class array_05 {
             }
         }
 
-        reverseArray(arr,index+1,n-1);
+        reverseArray(arr, index + 1, n - 1);
 
         return arr;
     }
-    public static void reverseArray(int[] arr, int l, int r){
 
-        if(l>=r) return;
+    public static void reverseArray(int[] arr, int l, int r) {
+
+        if (l >= r) return;
         int temp = arr[l];
         arr[l] = arr[r];
         arr[r] = temp;
 
-        reverseArray(arr,l+1, r-1);
+        reverseArray(arr, l + 1, r - 1);
     }
+
     public static void main(String[] args) {
-        int[] arr = {2,3,5,4,1,0,0};
+        int[] arr = {2, 3, 5, 4, 1, 0, 0};
         nextPermutation(arr);
-        for(int i: arr){
-            System.out.print(i+" ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
