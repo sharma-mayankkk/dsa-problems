8112027b8f5455321b3808613a7e9695a43042c5
sharma-mayankkk <br10216030515@gmail.com>
2026-04-14 00:05:08 +0530

    Longest Consecutive Sequence in an Array. better Approach

--- src/STRIVERaTOz/Array/Medium/array_07.java
+++ src/STRIVERaTOz/Array/Medium/array_07.java
@@ -4,6 +4,10 @@ package STRIVERaTOz.Array.Medium;
 //Problem Statement: Given an array nums of n integers.
 //Return the length of the longest sequence of consecutive integers. The integers in this sequence can appear in any order.
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class array_07 {
     public static boolean linearSearch(int[] arr, int target){
        for(int i: arr){
@@ -26,8 +30,37 @@ public class array_07 {
         }
         return longest;
     }
+
+    //Better Solution:
+    public static int longestSeq(int[] arr){
+        Arrays.sort(arr);
+        int longest = 1, currCount = 0, lastSmaller = Integer.MIN_VALUE;
+        for(int i: arr){
+            if(i-1 == lastSmaller){
+                currCount+=1;
+                lastSmaller = i;
+            } else if (i != lastSmaller) {
+                currCount = 1;
+                lastSmaller = i;
+            }
+            longest = Math.max(longest,currCount);
+        }
+
+        return longest;
+    }
+    //Optimal solution:(incomplete)
+
+//    public static int longestSequence2(int[] arr){
+//        if(arr.length ==0) return 0;
+//        int longest = 1;
+//        Set<Integer> st = new HashSet<>();
+//
+//        for(int i = 0; i<arr.length; i++){
+//
+//        }
+//    }
     public static void main(String[] args) {
-        int[] arr ={1,2,7,4,9,8,3};
-        System.out.println(longestSequence(arr));
+        int[] arr ={1,2,7,1,1,2,2,4,9,8,3};
+        System.out.println(longestSeq(arr));
     }
 }
