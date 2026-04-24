f2c96f25fafea0318d563d8655ad46946baad109
sharma-mayankkk <br10216030515@gmail.com>
2026-04-24 08:05:14 +0530

    updated with optimal solution;

--- src/STRIVERaTOz/Array/Hard/array_01.java
+++ src/STRIVERaTOz/Array/Hard/array_01.java
@@ -1,4 +1,5 @@
 package STRIVERaTOz.Array.Hard;
+
 import java.util.*;
 //Majority Elements(&gt;N/3 times) | Find the elements that appears more than N/3 times in the array
 //Problem Statement: Given an integer array nums of size n. Return all elements which appear more than n/3 times in the array. The output can be returned in any order.
@@ -40,8 +41,43 @@ public class array_01 {
         return list;
     }
 
+    //Optimal Solution:
+    public static ArrayList<Integer> majorityElement3(int[] arr) {
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+
+        for (int i : arr) {
+            if (cnt1 == 0 && i != ele2) {
+                ele1 = i;
+                cnt1++;
+            } else if (cnt2 == 0 && i != ele1) {
+                ele2 = i;
+                cnt2++;
+            } else if (ele1 == i) {
+                cnt1++;
+            } else if (ele2 == i) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int i : arr) {
+            if (ele1 == i) cnt1++;
+            if (ele2 == i) cnt2++;
+        }
+        int min = arr.length / 3 + 1;
+        if (cnt1 >= min) ans.add(ele1);
+        if (cnt2 >= min && ele2 != ele1) ans.add(ele2);
+
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3};
-        System.out.println(majorityElement2(arr));
+        int[] arr = {3, 3, 4, 2, 3, 3, 5, 3, 6, 3, 7, 3, 8, 3, 9, 3, 10, 3, 11, 3, 12, 3, 13, 3, 14, 3, 15, 3, 16, 3};
+        System.out.println(majorityElement3(arr));
     }
 }
