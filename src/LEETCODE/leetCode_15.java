793f99dbe8fdbdb121de0a592c581d1f6d600394
sharma-mayankkk <br10216030515@gmail.com>
2026-04-27 00:30:05 +0530

    LeetCode : 15 : medium : optimal solution

---
+++ src/LEETCODE/leetCode_15.java
@@ -0,0 +1,37 @@
+package LEETCODE;
+import java.util.*;
+
+//Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+//Notice that the solution set must not contain duplicate triplets.
+
+public class leetCode_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> ans = new HashSet<>();
+        int n = nums.length;
+        for(int i =0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int j = i+1;
+            int k= n-1;
+
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }else{
+                    List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+}
