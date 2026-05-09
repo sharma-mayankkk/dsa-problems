ecaaa73323dfd1b7b225eb225737b4c52cfe24b0
sharma-mayankkk <br10216030515@gmail.com>
2026-05-10 00:14:55 +0530

    Selection Sort

--- src/STRIVERaTOz/Sorting/selectionSort.java
+++ src/STRIVERaTOz/Sorting/selectionSort.java
@@ -1,10 +1,12 @@
 package STRIVERaTOz.Sorting;
 
+import java.util.Arrays;
+
 public class selectionSort {
     public static void selection(int[] arr){
-        for(int i=0; i<arr.length-2;i++){
+        for(int i=0; i<=arr.length-2;i++){
             int min = i;
-            for(int j=i;j<arr.length-1;j++){
+            for(int j=i;j<arr.length;j++){
                 if(arr[j]<arr[min]){
                     min = j;
                 }
@@ -15,6 +17,10 @@ public class selectionSort {
         }
     }
     public static void main(String[] args) {
-
+        int[] arr = {2,1,4,6,2,3,1};
+        selection(arr);
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
     }
 }
