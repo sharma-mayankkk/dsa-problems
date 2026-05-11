60681afd68909f4aeb53f43e5d5e0e63cbaeaa87
sharma-mayankkk <br10216030515@gmail.com>
2026-05-11 19:56:41 +0530

    recursive bubble sort

--- src/STRIVERaTOz/Sorting/bubbleSort.java
+++ src/STRIVERaTOz/Sorting/bubbleSort.java
@@ -13,6 +13,29 @@ public class bubbleSort {
         }
     }
 
+    //recursive bubble sort:
+    static void recursiveBubbleSort(int[] arr, int n) {
+
+        // Base case
+        if (n == 1) {
+            return;
+        }
+
+        // One pass of bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+
+                // Swap elements
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        // Recursive call for remaining array
+        recursiveBubbleSort(arr, n - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 1, 4, 6, 2, 3, 1};
         bubbleShot(arr);
