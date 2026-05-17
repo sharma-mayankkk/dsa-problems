9a80959218a17ee6d37a0a57ce947f843b6ab029
sharma-mayankkk <br10216030515@gmail.com>
2026-05-17 09:54:43 +0530

    LeetCode : 34 : medium : optimal

---
+++ src/LEETCODE/leetCode_34.java
@@ -0,0 +1,40 @@
+package LEETCODE;
+
+//Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+//If target is not found in the array, return [-1, -1].
+//You must write an algorithm with O(log n) runtime complexity.
+public class leetCode_34 {
+    public static int lowerBound(int[] arr, int x) {
+        int low = 0, high = arr.length - 1, ans = arr.length;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int upperBound(int[] arr, int x) {
+        int low = 0, high = arr.length - 1, ans = arr.length;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int lb = lowerBound(nums, target);
+        if (lb == nums.length || nums[lb] != target) return new int[]{-1, -1};
+        return new int[]{lb, upperBound(nums, target) - 1};
+    }
+}
