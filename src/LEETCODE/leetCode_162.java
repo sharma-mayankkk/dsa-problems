719c440a2647dd5b5e24c0e3365b0f8e833837cd
sharma-mayankkk <br10216030515@gmail.com>
2026-05-27 12:49:04 +0530

    leetcode : 162 : medium: find peak element

---
+++ src/LEETCODE/leetCode_162.java
@@ -0,0 +1,27 @@
+package LEETCODE;
+
+//A peak element is an element that is strictly greater than its neighbors.
+//Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+public class leetCode_162 {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int low = 1, high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[mid - 1]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
