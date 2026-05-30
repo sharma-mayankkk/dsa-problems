e7b066e328f508a79aa28cd4f5cac75234664e19
sharma-mayankkk <br10216030515@gmail.com>
2026-05-30 21:28:34 +0530

    finding nth root by BS

--- src/STRIVERaTOz/BinarySearching/BSonAnswers/array_01.java
+++ src/STRIVERaTOz/BinarySearching/BSonAnswers/array_01.java
@@ -13,8 +13,37 @@ public class array_01 {
             // If exceeds m, break
             if (power > m) break;
         }
-
         // If not found, return -1
         return -1;
+    }
+
+    public static double multiply(double n, double num) {
+        double ans = 1.0;
+        for (int i = 1; i <= n; i++) {
+            ans *= num;
+        }
+
+        return ans;
+    }
+
+    //using binary search:
+    public static double NthRoot(double n, double m) {
+        double low = 1;
+        double high = m;
+        double eps = 1e-6;
+
+        while ((high - low) > eps) {
+            double mid = (low + high) / 2.0;
+            if (multiply(n, mid) > m) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NthRoot(3, 27));
     }
 }
