0eba60fbda955595b71249f4ccf6f240e460585f
sharma-mayankkk <br10216030515@gmail.com>
2026-06-11 13:04:14 +0530

    LeetCode : 74: Medium: Search a 2D Matrix

---
+++ src/LEETCODE/leetCode_74.java
@@ -0,0 +1,33 @@
+package LEETCODE;
+
+//You are given an m x n integer matrix  with the following two properties:
+//
+//Each row is sorted in non-decreasing order.
+//The first integer of each row is greater than the last integer of the previous row.
+//Given an integer target, return true if target is in matrix or false otherwise.
+//
+//You must write a solution in O(log(m * n)) time complexity.
+public class leetCode_74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length, m = matrix[0].length;
+        int low = 0, high = n * m - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int row = mid / m, col = mid % m;
+
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
