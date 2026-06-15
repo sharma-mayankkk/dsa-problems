850d615cb7bd3230ed64883cfbaa219c4d57c840
sharma-mayankkk <br10216030515@gmail.com>
2026-06-15 17:02:10 +0530

    Largest Odd Number in String

---
+++ src/STRIVERaTOz/Strings/string_02.java
@@ -0,0 +1,20 @@
+package STRIVERaTOz.Strings;
+
+//Largest odd substring in string.
+//You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+//A substring is a contiguous sequence of characters within a string.
+public class string_02 {
+    public static String largestOdd(String s) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if ((s.charAt(i) - '0') % 2 == 1) {
+                return s.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String s = "3577834656";
+        System.out.println(largestOdd(s));
+    }
+}
