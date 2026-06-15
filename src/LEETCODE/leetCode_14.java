193a3206eb4581eb4d5e02df3404410a677723b6
sharma-mayankkk <br10216030515@gmail.com>
2026-06-15 14:10:33 +0530

    LeetCode: 14 : Easy : Longest Common Prefix

---
+++ src/LEETCODE/leetCode_14.java
@@ -0,0 +1,35 @@
+package LEETCODE;
+
+//Write a function to find the longest common prefix string amongst an array of strings.
+//
+//If there is no common prefix, return an empty string "".
+public class leetCode_14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        int preLen = 0;
+        while (true) {
+            boolean allGood = true;
+            if (preLen < strs[0].length()) {
+                char ch = strs[0].charAt(preLen);
+                for (int i = 1; i < strs.length; i++) {
+                    if (!(strs[i].length() > preLen && strs[i].charAt(preLen) == ch)) {
+                        allGood = false;
+                        break;
+                    }
+                }
+                if (allGood) {
+                    preLen++;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return strs[0].substring(0, preLen);
+    }
+}
