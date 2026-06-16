ed537801e972089163194c12e402a4e882f509db
sharma-mayankkk <br10216030515@gmail.com>
2026-06-16 21:46:16 +0530

    leetCode: 1021: easy: Remove outermost parenthesis

---
+++ src/LEETCODE/leetCode_1021.java
@@ -0,0 +1,24 @@
+package LEETCODE;
+
+public class leetCode_1021 {
+    public String removeOuterParentheses(String s) {
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+
+        for(char ch: s.toCharArray()){
+            if(ch == ')'){
+                count--;
+            }
+
+            if(count !=0){
+                ans.append(ch);
+            }
+
+            if(ch == '('){
+                count++;
+            }
+        }
+
+        return ans.toString();
+    }
+}
