ba152000e02ebbf7cae3923a6a217c999828cd7d
sharma-mayankkk <br10216030515@gmail.com>
2026-06-23 14:45:29 +0530

    LeetCode: 921: medium: Minimum Add to Make Parentheses Valid

---
+++ src/LEETCODE/leetCode_921.java
@@ -0,0 +1,28 @@
+package LEETCODE;
+
+//A parentheses string is valid if and only if:
+//
+//It is the empty string,
+//It can be written as AB (A concatenated with B), where A and B are valid strings, or
+//It can be written as (A), where A is a valid string.
+//You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+//
+//For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+//Return the minimum number of moves required to make s valid.
+public class leetCode_921 {
+    public int minAddToMakeValid(String s) {
+        int count = 0, opening = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                count++;
+            }
+            if (ch == ')' && count > 0) {
+                count--;
+            } else if (ch == ')' && count == 0) {
+                opening++;
+            }
+        }
+        return count + opening;
+    }
+}
