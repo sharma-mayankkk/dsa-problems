48b43e35c4aa80e55e2282b11f8a90ea09642332
sharma-mayankkk <br10216030515@gmail.com>
2026-07-03 23:07:39 +0530

    LeetCode: 876: easy: find middle of linked list.

---
+++ src/LEETCODE/leetCode_876.java
@@ -0,0 +1,32 @@
+package LEETCODE;
+
+public class leetCode_876 {
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
