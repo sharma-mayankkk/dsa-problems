fcdd9e66399fc2b2ab6f0f216ca1f9fc0a065eda
sharma-mayankkk <br10216030515@gmail.com>
2026-07-03 23:07:04 +0530

    find middle of linked list Optimal solution:

--- src/STRIVERaTOz/LinkedList/LL_02_FindMiddle.java
+++ src/STRIVERaTOz/LinkedList/LL_02_FindMiddle.java
@@ -11,7 +11,8 @@ public class LL_02_FindMiddle {
         }
     }
 
-    public static Node findMiddle(Node head){
+    //Bruteforce approach:
+     static Node findMiddle(Node head){
         Node temp = head;
         int count = 0;
         while(temp!=null){
@@ -27,16 +28,35 @@ public class LL_02_FindMiddle {
         return temp;
     }
 
+    //Optimal solution:
+     static Node findMiddle2(Node head){
+
+        Node fast = head, slow = head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
     public static void main(String[] args) {
         Node first = new Node(10);
         Node second = new Node(20);
         Node third = new Node(30);
+        Node fourth = new Node(40);
+        Node fifth = new Node(50);
+        Node sixth = new Node(60);
+        Node seventh = new Node(70);
 
         Node head = first;
         first.next = second;
         second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = seventh;
 
-        Node ans = findMiddle(first);
+
+        Node ans = findMiddle2(first);
         System.out.println(ans.data);
     }
 }
