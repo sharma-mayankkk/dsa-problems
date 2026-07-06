7d5c11c89e6fd7c0c2da344933d001c9e436e929
sharma-mayankkk <br10216030515@gmail.com>
2026-07-06 21:31:57 +0530

    LeetCode: 142: easy: Linked List Cycle II

---
+++ src/LEETCODE/leetCode_142.java
@@ -0,0 +1,38 @@
+package LEETCODE;
+
+//Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+//
+//There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+//
+//Do not modify the linked list.
+public class leetCode_142 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
