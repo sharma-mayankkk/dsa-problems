1c5d2b2e893cbc02ded3229d629dcfcaff47ebba
sharma-mayankkk <br10216030515@gmail.com>
2026-07-07 21:33:23 +0530

    find Length of the loop: Bruteforce method

---
+++ src/STRIVERaTOz/LinkedList/SinglyLL/LL_06_lengthOfLoop.java
@@ -0,0 +1,52 @@
+package STRIVERaTOz.LinkedList.SinglyLL;
+
+import java.util.HashMap;
+
+public class LL_06_lengthOfLoop {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static int lengthOfLoop(Node head) {
+        HashMap<Node, Integer> map = new HashMap<>();
+        int timer = 1;
+        Node temp = head;
+        while (temp != null) {
+            if (map.containsKey(temp)) {
+                int value = map.get(temp);
+                return timer - value;
+            }
+            map.put(temp, timer);
+            timer++;
+            temp = temp.next;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Node first = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node fourth = new Node(40);
+        Node fifth = new Node(50);
+        Node sixth = new Node(60);
+        Node seventh = new Node(70);
+
+        Node head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = seventh;
+        seventh.next = third;
+
+        System.out.println(lengthOfLoop(head));
+    }
+}
