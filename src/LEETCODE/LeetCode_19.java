6ecec661b9d4170758165adb7768dc62e7a690b2
sharma-mayankkk <br10216030515@gmail.com>
2026-07-09 21:11:22 +0530

    LeetCode: 19: Medium: Remove Nth Node From End of List

---
+++ src/LEETCODE/LeetCode_19.java
@@ -0,0 +1,38 @@
+package LEETCODE;
+
+//Given the head of a linked list, remove the nth node from the end of the list and return its head.
+public class LeetCode_19 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) return head.next;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
