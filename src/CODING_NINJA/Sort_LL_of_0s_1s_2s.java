b3da0a4293809f7902627da2d503d7aa7decfcd3
sharma-mayankkk <br10216030515@gmail.com>
2026-07-12 21:04:10 +0530

    coding Ninjas: easy:  Sort linked list of 0s 1s 2s

---
+++ src/CODING_NINJA/Sort_LL_of_0s_1s_2s.java
@@ -0,0 +1,60 @@
+package CODING_NINJA;
+
+//Problem statement
+//Given a linked list of 'N' nodes, where each node has an integer value that can be 0, 1, or 2. You need to sort the linked list in non-decreasing order and the return the head of the sorted list.
+public class Sort_LL_of_0s_1s_2s {
+    static class Node {
+        public int data;
+        public Node next;
+
+        Node() {
+            this.data = 0;
+            this.next = null;
+        }
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static Node sortList(Node head) {
+        // Write your code here
+
+        if (head == null || head.next == null) return head;
+
+        Node temp = head;
+
+        Node zeroHead = new Node(-1);
+        Node oneHead = new Node(-1);
+        Node twoHead = new Node(-1);
+
+
+        Node zero = zeroHead, one = oneHead, two = twoHead;
+
+        while (temp != null) {
+            if (temp.data == 0) {
+                zero.next = temp;
+                zero = zero.next;
+            } else if (temp.data == 1) {
+                one.next = temp;
+                one = one.next;
+            } else {
+                two.next = temp;
+                two = two.next;
+            }
+            temp = temp.next;
+        }
+
+        zero.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+
+        return zeroHead.next;
+    }
+}
